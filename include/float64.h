dba2143a551fded2ca9a9fdd3f17c1d08c23cb6f
ivanovandrey78 <jaydenpull1@gmail.com>
2026-01-01 16:15:33 +0300

    feat: base float64 struct

--- include/float64.h
+++ include/float64.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <cstring>
+#include <fstream>
+#include <string>
+
+class float64_t final {
+private:
+    char data_[8];
+
+public: 
+    // Constructors
+    float64_t() = default;
+    float64_t(const char* data);
+    float64_t(int data);
+    float64_t(const std::string& data);
+
+    float64_t(const float64_t& other);
+    float64_t& operator=(const float64_t& other);
+
+    float64_t(float64_t&& other) noexcept;
+    float64_t& operator=(float64_t&& other);
+
+    // Destructor
+    ~float64_t() = default;
+
+    // Operators
+    float64_t operator+(float64_t& other);
+    float64_t operator-(float64_t& other);
+    float64_t operator*(float64_t& other);
+    float64_t operator/(float64_t& other);
+    bool operator==(float64_t& other);
+
+    friend std::ostream& operator<<(std::ostream& os, const float64_t& num);
+    friend float64_t& operator>>(std::istream& is, float64_t& num);
+};
