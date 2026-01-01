d1402c953b0a2f87c37faac83d8a189a0785a1d6
ivanovandrey78 <jaydenpull1@gmail.com>
2026-01-01 15:57:11 +0300

    feat: base float32 interface

--- include/float32.h
+++ include/float32.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <cstring>
+#include <fstream>
+#include <string>
+
+class float32_t final {
+private:
+    char data_[4];
+
+public: 
+    // Constructors
+    float32_t() = default;
+    float32_t(const char* data);
+    float32_t(int data);
+    float32_t(const std::string& data);
+
+    float32_t(const float32_t& other);
+    float32_t& operator=(const float32_t& other);
+
+    float32_t(float32_t&& other) noexcept;
+    float32_t& operator=(float32_t&& other);
+
+    // Destructor
+    ~float32_t() = default;
+
+    // Operators
+    float32_t operator+(float32_t& other);
+    float32_t operator-(float32_t& other);
+    float32_t operator*(float32_t& other);
+    float32_t operator/(float32_t& other);
+    bool operator==(float32_t& other);
+
+    friend std::ostream& operator<<(std::ostream& os, const float32_t& num);
+    friend float32_t& operator>>(std::istream& is, float32_t& num);
+};
