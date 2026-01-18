5474846e6e6eec1c0c65f527f98d834b4ea778e3
ivanovandrey78 <jaydenpull1@gmail.com>
2026-01-19 00:51:09 +0300

    style(headers): change tab size

--- include/float32.h
+++ include/float32.h
@@ -6,37 +6,37 @@
 
 class float32_t final {
 private:
-    char data_[4];
+  char data_[4];
 
-public: 
-    // Constructors
-    float32_t() = default;
-    float32_t(int data);
-    float32_t(const char* data);
-    float32_t(const std::string& data);
+public:
+  // Constructors
+  float32_t() = default;
+  float32_t(int data);
+  float32_t(const char* data);
+  float32_t(const std::string& data);
 
-    float32_t(const float32_t& other);
-    float32_t& operator=(const float32_t& other);
+  float32_t(const float32_t& other);
+  float32_t& operator=(const float32_t& other);
 
-    float32_t(float32_t&& other) noexcept;
-    float32_t& operator=(float32_t&& other);
+  float32_t(float32_t&& other) noexcept;
+  float32_t& operator=(float32_t&& other);
 
-    // Destructor
-    ~float32_t() = default;
+  // Destructor
+  ~float32_t() = default;
 
-    // Unary Operators
-    float32_t operator+=(const float32_t& other);
-    float32_t operator-=(const float32_t& other);
-    float32_t operator*=(const float32_t& other);
-    float32_t operator/=(const float32_t& other);
+  // Unary Operators
+  float32_t operator+=(const float32_t& other);
+  float32_t operator-=(const float32_t& other);
+  float32_t operator*=(const float32_t& other);
+  float32_t operator/=(const float32_t& other);
 
-    // Operators
-    float32_t operator+(const float32_t& other);
-    float32_t operator-(const float32_t& other);
-    float32_t operator*(const float32_t& other);
-    float32_t operator/(const float32_t& other);
-    bool operator==(const float32_t& other);
+  // Operators
+  float32_t operator+(const float32_t& other);
+  float32_t operator-(const float32_t& other);
+  float32_t operator*(const float32_t& other);
+  float32_t operator/(const float32_t& other);
+  bool operator==(const float32_t& other);
 
-    friend std::ostream& operator<<(std::ostream& os, const float32_t& num);
-    friend float32_t& operator>>(std::istream& is, float32_t& num);
+  friend std::ostream& operator<<(std::ostream& os, const float32_t& num);
+  friend float32_t& operator>>(std::istream& is, float32_t& num);
 };
